b084c057d8d3f01fe81ceb5cfa1f24262ad0d496
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-10-23 18:48:12 -0400

    admin console

---
+++ components/navbars/adminNavbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Link from "next/link";
+import firebase from "firebase";
+import { useRouter } from "next/router";
+import "firebase/auth";
+export default function AdminNavbar({ delegateName }) {
+  const Router = useRouter();
+
+  const signOut = () => {
+    firebase.auth().signOut();
+    Router.push("/");
+  };
+  return (
+    <div>
+      <nav className="h-32 bg-main grid-cols-5 grid">
+        <Link href="/">
+          <a className="flex justify-center items-center ">
+            <h1 className="text-5xl font-bold text-white font-main">Munkey</h1>
+          </a>
+        </Link>
+
+        <button
+          className="col-start-5  font-main bg-white my-10 mx-32 rounded-lg"
+          onClick={signOut}
+        >
+          Logout
+        </button>
+      </nav>
+    </div>
+  );
+}
--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -5,8 +5,34 @@ import {
   AuthAction,
   getFirebaseAdmin,
 } from "next-firebase-auth";
+import AdminNavbar from "../../components/navbars/adminNavbar";
+import Link from "next/link";
 
-const Dashboard = () => {};
+const Dashboard = () => {
+  return (
+    <div>
+      <AdminNavbar></AdminNavbar>
+      <main>
+        <ul className="space-y-4">
+          <Link href="/admin/register/participant">
+            <a>
+              <li className="py-8 bg-gray-50 rounded-full m-8 pl-4 font-semibold text-lg">
+                Register New Users
+              </li>
+            </a>
+          </Link>
+          <Link href="/admin/register/manager">
+            <a>
+              <li className="py-8 bg-gray-50 rounded-full m-8 pl-4 font-semibold text-lg">
+                Register Crisis Managers
+              </li>
+            </a>
+          </Link>
+        </ul>
+      </main>
+    </div>
+  );
+};
 
 const Loader = () => {};
 
